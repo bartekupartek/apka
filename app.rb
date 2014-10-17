7195b2f9b57eb4032ed7bccfbb07183e99ecc765
bartekupartek <bartekupartek@gmail.com>
2014-10-17 12:14:05 +0200

    improve safety and dirs serving

--- app.rb
+++ app.rb
@@ -1,18 +1,39 @@
 require 'rubygems'
 require 'sinatra'
 
+set :public_folder, File.dirname(__FILE__) + '/katalog'
+
+def directory_exists? dir
+  Dir.exists? dir
+end
+
+def dir_entries dir
+  content = ''
+  Dir.foreach dir do |item|
+    next if item == '.' or item == '..'
+    content << "<p>#{item}</p>"
+  end
+  content
+end
+
 get '/' do
   "<h2>jestem!</h2>"
 end
 
-get '/katalog' do 
-  Dir.entries('katalog').reject { |d| d =~ /^\.{1,2}$/ }.map { |d| "<p><a href=\"/katalog/#{d}\">#{d}</a></p>" }
+get '/katalog' do
+  dir_entries settings.public_folder
 end
 
-get "/katalog/:file" do
-  if File.exists?("katalog/"+params[:file])
-    return File.open("katalog/" + params[:file])
+get '/katalog/:dir' do
+  dir = File.join settings.public_folder, params[:dir]
+  if directory_exists? dir
+    if Dir[File.join dir, '*'].count > 0
+      dir_entries dir
+    else
+      "pusto..."
+    end
   else
-   return "pusto..."
+    status 404
+    erb :oops
   end
 end
